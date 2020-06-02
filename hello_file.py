249288f8664e5b5855b0e9352ffe634fb7b72287
chak88 <88chak88@gmail.com>
2020-06-02 13:09:48 +0300

    initial2

--- hello_file.py
+++ hello_file.py
@@ -1,1 +1,16 @@
-print('hello')
+import requests
+
+api_url="http://api.openweathermap.org/data/2.5/weather"
+city=input('Введите название города : ')
+params={'q': city, "appid":"11c0d3dc6093f7442898ee49d2430d20" , 'units':'metric'}
+
+
+
+res=requests.get(api_url ,params =params )
+
+data=res.json()
+
+template='Температура в городе {} сейчас {}'
+
+
+print(template.format(city,data['main']['temp']))
